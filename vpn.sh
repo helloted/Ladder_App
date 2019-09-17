ab921006006113193fa417a7cdd0d5b05d904cd8
haozhicao <helloted@live.com>
2019-09-17 22:46:45 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -14,7 +14,7 @@ echo "\"local_port\":1080," >> /etc/shadowsocks.json
 echo "\"password\":\"HT_VPN_$\"," >> /etc/shadowsocks.json
 echo "\"timeout\":300," >> /etc/shadowsocks.json
 echo "\"method\":\"camellia-192-cfb\"," >> /etc/shadowsocks.json
-echo "\"fast_open\": false," >> /etc/shadowsocks.json
+echo "\"fast_open\": false" >> /etc/shadowsocks.json
 echo "}" >> /etc/shadowsocks.json
 
 firewall-cmd --zone=public --add-port=443/tcp --permanent
